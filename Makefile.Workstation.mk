a9a64e4a014c6831851e3363fd31cb0e40a18fe2
developer <developer@fatporkrinds.com>
2024-03-23 01:56:35 -0500

    Fixed a problem with .bashrc

--- Makefile.Workstation.mk
+++ Makefile.Workstation.mk
@@ -99,21 +99,38 @@ update-shell-config:
 		echo 'eval "$$(/usr/local/bin/brew shellenv)"' >> ~/.bashrc; \
 		echo 'eval "$$(/usr/local/bin/brew shellenv)"' >> ~/.zshrc; \
 	fi
-	@if [ -f "/home/$$USER/.bashrc" ]; then \
+	@if [ -f "/home/$(USER)/.bashrc" ]; then \
 		sudo echo 'eval "$$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc; \
 	fi
-	@if [ -f "/home/$$USER/.zshrc" ]; then \
+	@if [ -f "/home/$(USER)/.zshrc" ]; then \
 		sudo echo 'eval "$$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc; \
 	fi
-	@if [ ! -f "/usr/local/opt/go/bin" ]; then \
+	@if [ ! -d "/usr/local/opt/go/bin" ]; then \
 		echo 'export PATH="/usr/local/opt/go/bin:$${PATH}"' >> ~/.bashrc; \
 		echo 'export PATH="/usr/local/opt/go/bin:$${PATH}"' >> ~/.zshrc; \
 	fi
-	@if [ ! -f "/usr/local/opt/rust/bin" ]; then \
+	@if [ ! -d "/usr/local/opt/rust/bin" ]; then \
 		echo 'export PATH="/usr/local/opt/rust/bin:$${PATH}"' >> ~/.bashrc; \
 		echo 'export PATH="/usr/local/opt/rust/bin:$${PATH}"' >> ~/.zshrc; \
 	fi
+	# Additions for GOPATH, GOBIN, and GOROOT
+	@if [ ! -d "/usr/local/opt/go/bin" ]; then \
+		echo 'export GOPATH=$$HOME/go' >> ~/.bashrc; \
+		echo 'export GOPATH=$$HOME/go' >> ~/.zshrc; \
+		echo 'export GOBIN=$$GOPATH/bin' >> ~/.bashrc; \
+		echo 'export GOBIN=$$GOPATH/bin' >> ~/.zshrc; \
+		echo 'export GOROOT="$$(brew --prefix golang)/libexec"' >> ~/.bashrc; \
+		echo 'export GOROOT="$$(brew --prefix golang)/libexec"' >> ~/.zshrc; \
+		echo 'export PATH=$$PATH:$$GOPATH/bin' >> ~/.bashrc; \
+		echo 'export PATH=$$PATH:$$GOPATH/bin' >> ~/.zshrc; \
+		echo 'export PATH=$$PATH:$$GOROOT/bin' >> ~/.bashrc; \
+		echo 'export PATH=$$PATH:$$GOROOT/bin' >> ~/.zshrc; \
+		echo 'export PATH=$$PATH:/usr/local/opt/go/bin' >> ~/.bashrc; \
+		echo 'export PATH=$$PATH:/usr/local/opt/go/bin' >> ~/.zshrc; \
+	fi
 	@echo 'source ~/.bashrc' >> ~/.zshrc
+
+
 		
 
 # Define the target to install amix/vimrc configuration for Vim
