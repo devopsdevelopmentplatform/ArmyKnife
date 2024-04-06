4c62779b6c36ff078a6bb8f1e5c4c9f4159c7509
developer <developer@fatporkrinds.com>
2024-04-05 19:58:36 -0500

    Renamed a few files and prepared for testing

--- install.sh
+++ install.sh
@@ -12,24 +12,32 @@ if ! command -v make &>/dev/null; then
     fi
     exit 1
 fi
+# Copy over .bashrc or .zshrc if it doesn't exist depending on if MacOS or Linux.
+# if [ "`uname`" = "Darwin" ]; then \
+#     cp support/.zshrc ~/.zshrc; \
+# else \
+#     cp support/.bashrc ~/.bashrc; \
+# fi
+
+
 
 # Copy env_sample.txt to .env if it doesn't exist
 if [ ! -f .env ]; then
     if [ -f env_sample.txt ]; then
         cp env_sample.txt .env
-        echo ".env file created from env_sample.txt."
+        echo ".env file created from env_sample.txt. Now is a good time to fill out username and email for Git Settings"
     else
         echo "env_sample.txt does not exist. Please ensure env_sample.txt is in the current directory."
         exit 1
     fi
 else
-    echo ".env file already exists."
+    echo ".env file already exists. Now is a good time to fill out username and email for Git Settings"
 fi
 
 # Function to run a Make target
 run_make_target() {
     target=$1
-    echo "Running make target: $target"
+    echo "Running make target: $target . You may be prompted for your password. You may need to run it multiple times until you see a Success message."
     make $target
     if [ $? -ne 0 ]; then
         echo "Failed to execute make target: $target"
